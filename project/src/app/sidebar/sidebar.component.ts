2c607a2f27f979978d290ca449b142146410c726
unknown <divyasriambati123@gmail.com>
2021-11-06 09:58:44 +0530

    data binding

--- project/src/app/sidebar/sidebar.component.ts
+++ project/src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  public suggestions :any[] | undefined
+
+  constructor(public _dataService: DataService ) { }
+
+
 
   ngOnInit(): void {
+
+    this.suggestions = this._dataService.recommendations
+    console.log(this._dataService.recommendations);
+
   }
 
 }
